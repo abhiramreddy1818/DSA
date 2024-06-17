6880e5ed878c9d43197bdbab9ebea68f58cfaa1f
M Abhiram Reddy <127981318+abhiramreddy1818@users.noreply.github.com>
2024-06-17 12:51:00 +0530

    Time: 0 ms (100%), Space: 39.8 MB (96.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,21 +2,11 @@ class Solution {
     public int climbStairs(int n) {
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        return find(n,dp);
-    }
-    public int find(int n,int dp[]){
-        int count=0;
-        if(n==0)
-         return 1;
-         if(dp[n]!=-1)
-         return dp[n];
-        if(n>=2){
-           count+=find(n-2,dp)+find(n-1,dp);
-        }
-        else{
-             count+=find(n-1,dp);
-        }
-        return dp[n]=count;
-        
+        dp[0]=1;
+        dp[1]=1;
+      for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+      }
+      return dp[n];
     }
 }
