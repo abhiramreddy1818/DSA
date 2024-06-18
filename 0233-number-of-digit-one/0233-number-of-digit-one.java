81676b0e3a24e041396d53a94219477bd6b92bf0
M Abhiram Reddy <127981318+abhiramreddy1818@users.noreply.github.com>
2024-06-19 00:25:47 +0530

    Time: 0 ms (100%), Space: 40.7 MB (9.69%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.java
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -1,15 +1,25 @@
 class Solution {
     public int countDigitOne(int n) {
-       int d=10;
-       int m=1;
-       int ans=0;
-       int temp=n;
-       while(temp>0){
-         ans+=(n/d)*m+Math.min(Math.max(n%d-m+1,0),m);
-         d=d*10;
-         m=m*10;
-         temp/=10;
-       }
-       return ans;
+        int temp = n;
+        int d = 10;
+        int m = 1;
+        int ans = 0;
+
+        while (temp > 0) {
+            int r = 0;
+            if (temp % 10 > 1)
+                r = m;
+            
+            if (temp % 10 == 1) {
+                r = n % m + 1;
+            }
+            
+            ans += (n / d) * m + r; 
+            m *= 10;
+            d *= 10;
+            temp /= 10;
+        }
+        
+        return ans;
     }
 }
