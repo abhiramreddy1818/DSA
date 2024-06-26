5331d3a61bd6f09b16a0aef357287d0aabe6b02b
M Abhiram Reddy <127981318+abhiramreddy1818@users.noreply.github.com>
2024-06-26 12:26:28 +0530

    Time: 0 ms (100%), Space: 40.8 MB (74.21%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
@@ -1,22 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public TreeNode bstToGst(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode u = root;
-        while (u != null) {
-            stack.push(u);
-            u = u.right;
-        }
-        int greater = 0;
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            greater += node.val;
-            node.val = greater;
-            node = node.left;
-            while (node != null) {
-                stack.push(node);
-                node = node.right;
-            }
-        }
+    public TreeNode bstToGst(TreeNode temp) {
+        Stack<TreeNode> stack= new Stack<>();
+        TreeNode root=temp;
+        if(root==null)
         return root;
+        
+        while(root!=null){
+            stack.push(root);
+            root=root.right;
+        }
+        int sum=0;
+        while(!stack.isEmpty()){
+            TreeNode curr=stack.pop();
+            sum+=curr.val;
+            curr.val=sum;
+            
+            curr=curr.left;
+             while(curr!=null){
+                stack.push(curr);
+                 curr=curr.right;
+             }
+        }
+        return temp;
     }
 }
