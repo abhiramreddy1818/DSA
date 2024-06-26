a118004cfdb040cb67d0247183631aff03876372
M Abhiram Reddy <127981318+abhiramreddy1818@users.noreply.github.com>
2024-06-26 14:49:56 +0530

    Time: 2490 ms (27.04%), Space: 40.6 MB (99.38%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,27 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
     public int minCut(String s) {
-       int dp[]=new int[s.length()];
-       Arrays.fill(dp,-1);
-       return  find(s,0,s.length(),dp)-1;
+       int dp[]=new int[s.length()+1];
+       int n=s.length();
+       dp[n]=0;
+       for(int i=n-1;i>=0;i--){
+         int min=Integer.MAX_VALUE;
+         for(int j=i;j<n;j++){
+             if(isPalindrome(i,j,s)){
+                int len=1+dp[j+1];
+                min=Math.min(min,len);
+             }
+              
+         }
+         dp[i]=min;
+       }
+       return dp[0]-1;
     }
-    public int find(String s, int i ,int n,int dp[]){
-        if(i==n)
-        return 0;
-        if(dp[i]!=-1)
-        return dp[i];
-   
-        int min=Integer.MAX_VALUE;
-        for(int j=i;j<n;j++){
-            
-           
-            if(isPalindrome(i,j,s)){
-            int len=1+find(s,j+1,n,dp);
-             min=Math.min(min,len);
-            }
-             
-        }
-        return dp[i]=min;
-    }
+    
     public boolean isPalindrome(int i, int j, String s) {
         while (i < j) {
             if (s.charAt(i) != s.charAt(j)) return false;
@@ -29,4 +26,5 @@ class Solution {
             j--;
         }
         return true;
-    }}
+    }
+}
