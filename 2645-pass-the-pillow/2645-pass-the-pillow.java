12509aaec4581692e7e95dd29ddfff631266d928
M Abhiram Reddy <127981318+abhiramreddy1818@users.noreply.github.com>
2024-07-06 19:45:13 +0530

    Time: 0 ms (100%), Space: 40.2 MB (46.33%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.java
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -3,9 +3,9 @@ class Solution {
          if(time<n){
             return time+1;
          }
-         int rounds=time/(n-1);
-         if(rounds%2==0)
-         return (time%(n-1));
+        
+         if((time/(n-1))%2==0)
+         return (time%(n-1)) +1;
          else
           return (n-time%(n-1));
     }
